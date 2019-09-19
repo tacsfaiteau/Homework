582d5fecd20ec71c7233213ab803319ccef9b587
tacsfaiteau <55443318+tacsfaiteau@users.noreply.github.com>
2019-09-19 08:42:06 -0400

    Add files via upload

---
+++ 2019-09-19-Homework/2019-09-19-Homework/main.cpp
@@ -0,0 +1,16 @@
+// Name: Omar Faiteau
+// Intro to Computer Science 
+// Period 1
+// Mr. Williams 
+// 2019-09-19 Homework
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Name: Omar Faiteau" << std::endl 
+		<<"Period 1 : Computer Science"
+		                                  
+
+
+}
